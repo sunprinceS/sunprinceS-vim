90a4c400700a43f300629fec81fa85b57a6befcb
TonyHsu <sunprince12014@gmail.com>
2015-04-10 00:02:08 +0800

    a little modified

--- CustomizedVim.sh
+++ CustomizedVim.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env sh
 
-CHOICE=$(
+CHOICES=$(
 zenity --list \
     --checklist \
     --height=400 --width=600 \
     --title="~Choose the package you want~" \
     --column "Choose" --column="PlugIn" --column="Description"\
     TRUE NerdTree "File Browser"\
-    TRUE TagBar "Class Browser"\
+    FALSE Vim-airline "Pretty-status"\
+    TRUE Tagbar "class/modeule browser"\
+    TRUE Bufexplorer "Fantasy buffer explorer"\
+    TRUE NerdCommenter "convenient comment"\
+    TRUE LaTeX-Box "Latex Development"\
+    FALSE Vimwiki "wiki Note"\
+    TRUE SrcExpl "Code insight"\
+    FALSE Vim-multiple-cursors "Multiple cursor"\
+    FALSE SingleCompile "Compile"\
+    FALSE Vim-matlab "Matlab"\
     FALSE Gitgutter "git management")
-
-echo $CHOICE
+#About the usage of for separator
+#http://stackoverflow.com/questions/918886/how-do-i-split-a-string-on-a-delimiter-in-bash
+IFS="|"
+for choice in $CHOICES
+do
+    echo $choice
+done
